ca736eadde608311a8acfcd4aa787e42a7f7bd1e
futzone20 <seufiltrodiario@gmail.com>
2026-07-24 17:59:59 -0300

    capitao.perfil.tsx

--- src/routes/capitao.perfil.tsx
+++ src/routes/capitao.perfil.tsx
@@ -1,12 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { PerfilCompleto } from "@/components/PerfilCompleto";
-import { CentralMensagensCard } from "@/components/CentralMensagensCard";
 
 export const Route = createFileRoute("/capitao/perfil")({
-  component: () => (
-    <div className="space-y-4">
-      <CentralMensagensCard />
-      <PerfilCompleto />
-    </div>
-  ),
+  component: PerfilCompleto,
 });
